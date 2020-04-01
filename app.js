3ba1bc00c9959f096d1141e596557de1ac668485
ketan7705 <hi@example.com>
2020-04-01 14:11:33 +0530

    Changes

--- app.js
+++ app.js
@@ -34,11 +34,11 @@ app.post("/",function(req,res){
     console.log(fName,lName,email); 
     
     var options = {
-// 5aafbfca83
-        url : "https://us19.api.mailchimp.com/3.0/lists/5aafbfca83/members/6f4b546d2ba9440271a9f8777142bae0-us19/notes/",
+
+        url : "https://us19.api.mailchimp.com/3.0/lists/--List-id--/members/--ApiKey--/notes/",
         method: "POST",
         headers :{
-            "Authorization" : "5aafbfca83 [6f4b546d2ba9440271a9f8777142bae0-us19]"
+            "Authorization" : "--List-id-- [--ApiKey--]"
         },
         body :jsonData
     };
@@ -67,8 +67,3 @@ app.listen(process.env.PORT || 3000 ,function(){
     console.log("Your Server Starts At Port 3000 ");
 });
 
-// API KEy
-// b7e3dd1582240b3bfd8be3f73f249ba4-us19
-
-//list id
-// 5aafbfca83
